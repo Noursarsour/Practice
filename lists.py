c6111dd28f2d0f909a7c30a54b4c63d0c9457530
Nour <noursarsour.ns@gmail.com>
2024-12-31 00:16:49 -0800

    more practice on list slicing

--- lists.py
+++ lists.py
@@ -46,3 +46,31 @@ print(ingredients)
 ingredients.sort() #modifies the list in place, it does not create a new list
 #do not set sort() to a variable, it is going to be none.
 print(ingredients)
+print(sorted(ingredients)) #it does not modify the original list
+
+
+#list slicing
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(nums[:])
+print(nums[:0]) #empty
+print(nums[:-1])
+print(nums[2:6])
+print(nums[6:])
+print(nums[:7])
+print(nums[::2]) #step
+print(nums[::-1])
+print(nums[-2:])
+print(nums[:-2])
+
+#range
+for number in range(5):
+    print(number)
+
+print(list(range(10)))
+print(range(10, 4))
+print(list(range(10, 2, -1)))
+
+for index, item in enumerate(ingredients):
+    print(index, item)
+for index in range(len(ingredients)):
+    print(index, ingredients[index])
